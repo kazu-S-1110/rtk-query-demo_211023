f9d2e6998f0e4ba004cbf70ab8abb1341c02ec5d
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-10-23 21:53:25 +0900

    取得したものを画面表示させてみた。これだけのコードでできるのがすごい。

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
 import './App.css';
+import { useGetPokemonByNameQuery } from './services/pokemon';
 
-function App() {
-  return <h1>test</h1>;
-}
+const App: React.VFC = () => {
+  const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur');
+  return (
+    <div className="app">
+      {error ? (
+        <h1>error!</h1>
+      ) : isLoading ? (
+        <h1>Loading...</h1>
+      ) : data ? (
+        <>
+          <h1>{data.species.name}</h1>
+          <img src={data.sprites.front_shiny} alt={data.species.name} />
+        </>
+      ) : null}
+    </div>
+  );
+};
 
 export default App;
